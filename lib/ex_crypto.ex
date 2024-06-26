a6519cdec19ac0380d614b1482f8e0f915b51df4
Steve Anderson <sanderson@sopost.com>
2024-06-26 11:12:51 +0100

    fix: Fix duplicate documentation warning

--- lib/ex_crypto.ex
+++ lib/ex_crypto.ex
@@ -31,7 +31,10 @@ defmodule ExCrypto do
   end
 
   defp test_key_and_iv_bitlength(nil), do: :ok
-  defp test_key_and_iv_bitlength({_key, iv}) when bit_size(iv) != @iv_bit_length, do: {:error, @bitlength_error}
+
+  defp test_key_and_iv_bitlength({_key, iv}) when bit_size(iv) != @iv_bit_length,
+    do: {:error, @bitlength_error}
+
   defp test_key_and_iv_bitlength({key, _iv}) when rem(bit_size(key), 128) == 0, do: :ok
   defp test_key_and_iv_bitlength({key, _iv}) when rem(bit_size(key), 192) == 0, do: :ok
   defp test_key_and_iv_bitlength({key, _iv}) when rem(bit_size(key), 256) == 0, do: :ok
@@ -275,13 +278,21 @@ defmodule ExCrypto do
   end
 
   @doc """
-  Encrypt a `binary` with AES in CBC mode providing explicit IV via map.
-
-  Returns a tuple containing the `initialization_vector`, and `cipher_text`.
+  Encrypt a `binary` with AES in CBC mode.
 
   At a high level encryption using AES in CBC mode looks like this:
 
       key + clear_text + map -> init_vec + cipher_text
+
+  Can be called in one of two ways:
+
+  `encrypt(key, clear_text, %{initialization_vector: initialization_vector})`:
+
+  Will use the given IV, and return a tuple containing the `initialization_vector` and the `cipher_text`.
+
+  `encrypt(key, auth_data, clear_text)`:
+
+  Will generate a random 128-bit IV, and return a tuple containing the `initialization_vector`, the `cipher_text` and the `cipher_tag`.
 
   ## Examples
 
@@ -290,6 +301,16 @@ defmodule ExCrypto do
       iex> {:ok, init_vec} = ExCrypto.rand_bytes(16)
       iex> {:ok, {_iv, cipher_text}} = ExCrypto.encrypt(aes_256_key, clear_text, %{initialization_vector: init_vec})
       iex> assert(is_bitstring(cipher_text))
+      true
+
+      iex> clear_text = "my-clear-text"
+      iex> auth_data = "my-auth-data"
+      iex> {:ok, aes_256_key} = ExCrypto.generate_aes_key(:aes_256, :bytes)
+      iex> {:ok, {_ad, payload}} = ExCrypto.encrypt(aes_256_key, auth_data, clear_text)
+      iex> {_init_vec, cipher_text, cipher_tag} = payload
+      iex> assert(is_bitstring(cipher_text))
+      true
+      iex> assert(bit_size(cipher_tag) == 128)
       true
 
   """
@@ -303,25 +324,6 @@ defmodule ExCrypto do
     kind, error -> normalize_error(__STACKTRACE__, kind, error, {key, initialization_vector})
   end
 
-  @doc """
-  Same as `encrypt/4` except the `initialization_vector` is automatically generated.
-
-  A 128 bit `initialization_vector` is generated automatically by `encrypt/3`. It returns a tuple
-  containing the `initialization_vector`, the `cipher_text` and the `cipher_tag`.
-
-  ## Examples
-
-      iex> clear_text = "my-clear-text"
-      iex> auth_data = "my-auth-data"
-      iex> {:ok, aes_256_key} = ExCrypto.generate_aes_key(:aes_256, :bytes)
-      iex> {:ok, {_ad, payload}} = ExCrypto.encrypt(aes_256_key, auth_data, clear_text)
-      iex> {_init_vec, cipher_text, cipher_tag} = payload
-      iex> assert(is_bitstring(cipher_text))
-      true
-      iex> assert(bit_size(cipher_tag) == 128)
-      true
-
-  """
   @spec encrypt(binary, binary, binary) ::
           {:ok, {binary, {binary, binary, binary}}} | {:error, binary}
   def encrypt(key, authentication_data, clear_text) do
@@ -373,7 +375,8 @@ defmodule ExCrypto do
       true
 
   """
-  @spec decrypt(binary, binary, binary, binary, binary) :: {:ok, binary} | {:error, :decrypt_failed} | {:error, binary}
+  @spec decrypt(binary, binary, binary, binary, binary) ::
+          {:ok, binary} | {:error, :decrypt_failed} | {:error, binary}
   def decrypt(key, authentication_data, initialization_vector, cipher_text, cipher_tag) do
     _decrypt(key, initialization_vector, {authentication_data, cipher_text, cipher_tag}, :aes_gcm)
   end
@@ -395,10 +398,12 @@ defmodule ExCrypto do
       true
 
   """
-  @spec decrypt(binary, binary, binary) :: {:ok, binary} | {:error, :decrypt_failed} | {:error, binary}
+  @spec decrypt(binary, binary, binary) ::
+          {:ok, binary} | {:error, :decrypt_failed} | {:error, binary}
   def decrypt(key, initialization_vector, cipher_text) do
-    with {:ok, padded_cleartext} <- _decrypt(key, initialization_vector, cipher_text, :aes_cbc256),
-      do: {:ok, unpad(padded_cleartext)}
+    with {:ok, padded_cleartext} <-
+           _decrypt(key, initialization_vector, cipher_text, :aes_cbc256),
+         do: {:ok, unpad(padded_cleartext)}
   catch
     kind, error -> normalize_error(__STACKTRACE__, kind, error, {key, initialization_vector})
   end
@@ -481,6 +486,7 @@ defmodule ExCrypto do
     # otp 23 deprecated and otp 24 removed retired cipher names
     # http://erlang.org/doc/apps/crypto/new_api.html#retired-cipher-names
     defp map_algorithm(:aes_cbc256, _key), do: :aes_256_cbc
+
     defp map_algorithm(:aes_gcm, key) do
       case bit_size(key) do
         128 -> :aes_128_gcm
@@ -490,19 +496,46 @@ defmodule ExCrypto do
     end
 
     defp crypto_block_encrypt(algorithm, key, initialization_vector, {aad, plain_text}) do
-      :crypto.crypto_one_time_aead(map_algorithm(algorithm, key), key, initialization_vector, plain_text, aad, true)
+      :crypto.crypto_one_time_aead(
+        map_algorithm(algorithm, key),
+        key,
+        initialization_vector,
+        plain_text,
+        aad,
+        true
+      )
     end
 
     defp crypto_block_encrypt(algorithm, key, initialization_vector, plain_text) do
-      :crypto.crypto_one_time(map_algorithm(algorithm, key), key, initialization_vector, plain_text, true)
+      :crypto.crypto_one_time(
+        map_algorithm(algorithm, key),
+        key,
+        initialization_vector,
+        plain_text,
+        true
+      )
     end
 
     defp crypto_block_decrypt(algorithm, key, initialization_vector, {aad, data, tag}) do
-      :crypto.crypto_one_time_aead(map_algorithm(algorithm, key), key, initialization_vector, data, aad, tag, false)
+      :crypto.crypto_one_time_aead(
+        map_algorithm(algorithm, key),
+        key,
+        initialization_vector,
+        data,
+        aad,
+        tag,
+        false
+      )
     end
 
     defp crypto_block_decrypt(algorithm, key, initialization_vector, data) do
-      :crypto.crypto_one_time(map_algorithm(algorithm, key), key, initialization_vector, data, false)
+      :crypto.crypto_one_time(
+        map_algorithm(algorithm, key),
+        key,
+        initialization_vector,
+        data,
+        false
+      )
     end
   else
     defp crypto_block_encrypt(algorithm, key, initialization_vector, encryption_payload) do
